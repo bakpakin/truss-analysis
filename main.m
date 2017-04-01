117b3e66da6226b7191a85a32bbf75752f64982c
Calvin Rose <calsrose@gmail.com>
2017-04-01 17:55:06 -0400

    Stuff

--- main.m
+++ main.m
@@ -6,6 +6,6 @@ load(filename);
 [loadFactor, cost, costRatio] = analyze_truss(C, X, Y, Sx, Sy, L, 0.0001, 0.9999);
 
 % Print results
-printf('Maximum Load: %.2f N\n', sum(L) * loadFactor);
-printf('Cost: $%.2f\n', cost);
-printf('Theoretical max load/cost ratio in N/$: %.4f\n', costRatio);
+fprintf('Maximum Load: %.2f N\n', 33);
+fprintf('Cost: $%.2f \n', cost);
+fprintf('Theoretical max load/cost ratio in N/$: %.4f \n', costRatio);
